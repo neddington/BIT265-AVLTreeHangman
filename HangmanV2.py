cc3b2e06a38bef5058928a3f49718976125b4aa4
Nick Eddington <neddington@cascadia.edu>
2023-04-20 11:10:08 -0700

    BIT265-AVLTreeHangman

---
+++ HangmanV2.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+import random
+from avl_tree import AVLTree
+
+# Hardcoded list of words to choose from
+words = ["apple", "banana", "cherry", "durian", "elderberry", "cheese"]
+
+# Hangman figure
+hangman = [
+    " _____ ",
+    " |     |",
+    " O     |",
+    "/|\\   |",
+    "/ \\   |",
+    "       |",
+    "========="
+]
+
+
+class WordGuessingGame:
+    def __init__(self, master):
+        self.guess_label = None
+        self.hangman_label = None
+        self.buttons_frame = None
+        self.hangman_image = None
+        self.word_label = None
+        self.master = master
+        self.avl_tree = AVLTree()  # AVL tree to keep track of guessed letters
+        self.word = random.choice(words)  # Choose a random word from the list
+        self.word_state = ["_" for _ in range(len(self.word))]  # List to keep track of revealed letters
+        self.guesses = set()  # Set to keep track of all guesses (for displaying at end)
+        self.remaining_guesses = 6  # Number of incorrect guesses before losing
+        self.setup_ui()
+
+    def setup_ui(self):
+        # Create UI elements
+        self.word_label = tk.Label(self.master, text=" ".join(self.word_state))
+        self.word_label.pack()
+        self.guess_label = tk.Label(self.master, text=f"Remaining guesses: {self.remaining_guesses}")
+        self.guess_label.pack()
+        self.buttons_frame = tk.Frame(self.master)
+        self.buttons_frame.pack()
+        for letter in "abcdefghijklmnopqrstuvwxyz":
+            # Create a button for each letter
+            btn = tk.Button(self.buttons_frame, text=letter, command=lambda l=letter: self.make_guess(l))
+            btn.pack(side=tk.LEFT)
+        self.hangman_label = tk.Label(self.master, text="\n".join(hangman[:0]))
+        self.hangman_label.pack()
+        self.hangman_image = tk.Label(self.master, text="")
+        self.hangman_image.pack()
+
+    def make_guess(self, letter):
+        # Check if the letter has already been guessed
+        if letter in self.avl_tree:
+            return
+        self.avl_tree.insert(letter)  # Insert the letter into the AVL tree
+        self.guesses.add(letter)  # Add the letter to the set of all guesses
+        if letter in self.word:
+            # If the letter is in the word, reveal all occurrences of the letter
+            for i in range(len(self.word)):
+                if self.word[i] == letter:
+                    self.word_state[i] = letter
+            self.word_label.config(text=" ".join(self.word_state))
+            if "_" not in self.word_state:
+                # If there are no underscores left in the word, the player has won
+                self.end_game(True)
+        else:
+            # If the letter is not in the word, decrement the remaining guesses and update the UI
+            self.remaining_guesses -= 1
+            self.guess_label.config(text=f"Remaining guesses: {self.remaining_guesses}")
+            if self.remaining_guesses == 0:
+                # If there are no more remaining guesses, the player has lost
+                self.end_game(False)
+                hangman_images = [
+                    "  +----+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="
+                ]
+                self.hangman_image.config(text=hangman_images[0])
+            else:
+                # Update the hangman based on the remaining guesses
+                hangman_images = [
+                    "  +-----+\n  |   |\n      |\n      |\n      |\n      |\n========",
+                    "  +-----+\n  |   |\n  O   |\n      |\n      |\n      |\n========",
+                    "  +-----+\n  |   |\n  O   |\n  |   |\n      |\n      |\n========",
+                    "  +-----+\n  |   |\n  O   |\n /|   |\n      |\n      |\n========",
+                    "  +-----+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n========",
+                    "  +-----+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n========",
+                    "  +-----+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========"
+                ]
+                self.hangman_image.config(text=hangman_images[6 - self.remaining_guesses])
+
+    def end_game(self, win):
+        # Display the solution and all guesses, and disable all buttons
+        solution_label = tk.Label(self.master, text=f"The word was: {self.word}")
+        solution_label.pack()
+        guesses_label = tk.Label(self.master, text=f"Your guesses: {', '.join(self.guesses)}")
+        guesses_label.pack()
+        for btn in self.buttons_frame.winfo_children():
+            btn.config(state=tk.DISABLED)
+        if win:
+            # If the player won, display a message
+            message_label = tk.Label(self.master, text="Congratulations, you won!")
+            message_label.pack()
+        else:
+            # If the player lost, display a message
+            message_label = tk.Label(self.master, text="Sorry, you lost.")
+            message_label.pack()
+
+        # Reveal all correct letters in the word
+        revealed_word = ""
+        for i in range(len(self.word)):
+            if self.word[i] in self.avl_tree:
+                revealed_word += self.word[i] + " "
+            else:
+                revealed_word += "_ "
+        self.word_label.config(text=revealed_word)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = WordGuessingGame(root)
+    root.mainloop()
---
+++ avl_tree.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        self.root = self._insert(self.root, value)
+
+    def _insert(self, node, value):
+        if node is None:
+            return Node(value)
+        elif value < node.value:
+            node.left = self._insert(node.left, value)
+        else:
+            node.right = self._insert(node.right, value)
+
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+        balance = self._get_balance(node)
+
+        if balance > 1 and value < node.left.value:
+            return self._right_rotate(node)
+
+        if balance < -1 and value > node.right.value:
+            return self._left_rotate(node)
+
+        if balance > 1 and value > node.left.value:
+            node.left = self._left_rotate(node.left)
+            return self._right_rotate(node)
+
+        if balance < -1 and value < node.right.value:
+            node.right = self._right_rotate(node.right)
+            return self._left_rotate(node)
+
+        return node
+
+    def __contains__(self, value):
+        return self._contains(self.root, value)
+
+    def _contains(self, node, value):
+        if node is None:
+            return False
+        elif node.value == value:
+            return True
+        elif value < node.value:
+            return self._contains(node.left, value)
+        else:
+            return self._contains(node.right, value)
+
+    @staticmethod
+    def _get_height(node):
+        if node is None:
+            return 0
+        return node.height
+
+    def _get_balance(self, node):
+        if node is None:
+            return 0
+        return self._get_height(node.left) - self._get_height(node.right)
+
+    def _left_rotate(self, node):
+        new_root = node.right
+        node.right = new_root.left
+        new_root.left = node
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+        new_root.height = 1 + max(self._get_height(new_root.left), self._get_height(new_root.right))
+        return new_root
+
+    def _right_rotate(self, node):
+        new_root = node.left
+        node.left = new_root.right
+        new_root.right = node
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+        new_root.height = 1 + max(self._get_height(new_root.left), self._get_height(new_root.right))
+        return new_root
